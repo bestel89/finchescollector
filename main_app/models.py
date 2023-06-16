3c0a38b573c64430b805702aadacf8ee9c6e0af3
bestel89 <ben.stell1989@gmail.com>
2023-06-16 15:36:47 +0100

    end of lab 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Finch(models.Model):
@@ -12,97 +19,25 @@ class Finch(models.Model):
 
     def __str__(self):
         return f'{self.species} id:{self.id}'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id}) #Django equivalent of URL template tag
 
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
 
-finches = [
-    {
-        'species': 'Zebra Finch',
-        'color': 'Gray',
-        'size': 'Small',
-        'habitat': 'Australia',
-        'diet': 'Seeds',
-        'lifespan': 5,
-        'population': 'Common'
-    },
-    {
-        'species': 'Goldfinch',
-        'color': 'Yellow',
-        'size': 'Small',
-        'habitat': 'North America',
-        'diet': 'Seeds, Insects',
-        'lifespan': 10,
-        'population': 'Stable'
-    },
-    {
-        'species': 'Gouldian Finch',
-        'color': 'Red, Green, Yellow',
-        'size': 'Small',
-        'habitat': 'Australia',
-        'diet': 'Seeds, Insects',
-        'lifespan': 6,
-        'population': 'Endangered'
-    },
-    {
-        'species': 'Purple Finch',
-        'color': 'Red, Brown',
-        'size': 'Medium',
-        'habitat': 'North America',
-        'diet': 'Seeds, Insects',
-        'lifespan': 7,
-        'population': 'Stable'
-    },
-    {
-        'species': 'Society Finch',
-        'color': 'White, Brown',
-        'size': 'Small',
-        'habitat': 'Domesticated',
-        'diet': 'Seeds, Pellets',
-        'lifespan': 7,
-        'population': 'Common'
-    },
-    {
-        'species': 'Crimson Finch',
-        'color': 'Red',
-        'size': 'Small',
-        'habitat': 'Australia',
-        'diet': 'Seeds, Insects',
-        'lifespan': 6,
-        'population': 'Stable'
-    },
-    {
-        'species': 'European Goldfinch',
-        'color': 'Red, Black, White',
-        'size': 'Small',
-        'habitat': 'Europe, Asia, Africa',
-        'diet': 'Seeds, Insects',
-        'lifespan': 12,
-        'population': 'Stable'
-    },
-    {
-        'species': 'Java Sparrow',
-        'color': 'Gray',
-        'size': 'Small',
-        'habitat': 'Indonesia',
-        'diet': 'Seeds',
-        'lifespan': 7,
-        'population': 'Endangered'
-    },
-    {
-        'species': 'Bengalese Finch',
-        'color': 'White, Brown',
-        'size': 'Small',
-        'habitat': 'Domesticated',
-        'diet': 'Seeds, Insects',
-        'lifespan': 5,
-        'population': 'Common'
-    },
-    {
-        'species': 'Red-cheeked Cordon-bleu',
-        'color': 'Blue, Red',
-        'size': 'Small',
-        'habitat': 'Sub-Saharan Africa',
-        'diet': 'Seeds, Insects',
-        'lifespan': 5,
-        'population': 'Stable'
-    }
-]
+    finch = models.ForeignKey(
+        Finch,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"({self.get_meal_display()} on {self.date})"
+    
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request): 
@@ -16,6 +18,35 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
 	finch = Finch.objects.get(id=finch_id)
+	feeding_form = FeedingForm()
 	return render(request, 'finches/detail.html', {
-		'finch': finch
+		'finch': finch,
+		'feeding_form': feeding_form
 	})
+
+class FinchCreate(CreateView):
+	model = Finch
+	fields = '__all__' #auto generate inputs on the form for all cat inputs
+	# fields = ['name', 'breed', 'description', 'age'] - ALTERNATIVE IF WANT TO REORDER THE FIELDS
+	# success_url = '/cats' #redirect to the index page
+
+class FinchUpdate(UpdateView):
+	model = Finch
+	fields = '__all__'
+
+class FinchDelete(DeleteView):
+	model = Finch
+	success_url = '/finches'
+
+def add_feeding(request, finch_id):
+	#create a modelform instance using
+	#the data that was submitted in the form
+	form = FeedingForm(request.POST)
+	if form.is_valid():
+		#We want a model instance but we can't
+		#save to the DB yet
+		#because we haven't assigned the FK (cat id)
+		new_feeding = form.save(commit=False)
+		new_feeding.finch_id = finch_id
+		new_feeding.save()
+	return redirect('detail', finch_id=finch_id)
