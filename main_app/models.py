ab656a5890f3cf7e60481c74d52032c1144f263d
bestel89 <ben.stell1989@gmail.com>
2023-06-19 12:22:14 +0100

    end of lab 5

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
 
 # Create your models here.
 class Finch(models.Model):
@@ -16,6 +27,7 @@ class Finch(models.Model):
     diet = models.CharField(max_length=100)
     lifespan = models.IntegerField()
     population = models.CharField(max_length=100)
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return f'{self.species} id:{self.id}'
